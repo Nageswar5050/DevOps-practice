f5c8557db547210e6ad1947fa6d82c1396c08df1
Nageswar5050 <nageswar0550@gmail.com>
2024-02-04 15:42:17 +0530

    modified jenkinsfile

--- terraform/roboshop_dev/01_vpc/parameter.tf
+++ terraform/roboshop_dev/01_vpc/parameter.tf
@@ -1,29 +1,29 @@
 resource "aws_ssm_parameter" "vpc_id" {
-  type  = "String"
   name  = "/${var.project_name}/${var.environment}/vpc_id"
+  type  = "String"
   value = module.roboshop_vpc.main_vpc_id
 }
 
 resource "aws_ssm_parameter" "main_igw_id" {
-  type = "String"
   name = "/${var.project_name}/${var.environment}/vpc_id"
+  type = "String"
   value = module.roboshop_vpc.main_igw
 }
 
 resource "aws_ssm_parameter" "frontend_subnet_id" {
-  type  = "String"
   name  = "/${var.project_name}/${var.environment}/frontend_subnet_id"
+  type  = "String"
   value = module.roboshop_vpc.main_frontend_subnet_id
 }
 
 resource "aws_ssm_parameter" "backend_subnet_id" {
-  type  = "String"
   name  = "/${var.project_name}/${var.environment}/backend_subnet_id"
+  type  = "String"
   value = module.roboshop_vpc.main_backend_subnet_id
 }
 
 resource "aws_ssm_parameter" "database_subnet_id" {
-  type  = "String"
   name  = "/${var.project_name}/${var.environment}/database_subnet_id"
+  type  = "String"
   value = module.roboshop_vpc.main_database_subnet_id
 }
